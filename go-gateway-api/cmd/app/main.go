7a3f6bca45e4fa80777f7d2130ec1f0d93c30a0d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-04-07 14:15:05 -0300

    feat: implements cmd app main

--- go-gateway-api/cmd/app/main.go
+++ go-gateway-api/cmd/app/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rodolfoHOk/fullcycle.imersao22/go-gateway-api/internal/repository"
+	"github.com/rodolfoHOk/fullcycle.imersao22/go-gateway-api/internal/service"
+	"github.com/rodolfoHOk/fullcycle.imersao22/go-gateway-api/internal/web/server"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSLMODE", "disable"),
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	port := getEnv("HTTP_PORT", "8080")
+	srv := server.NewServer(accountService, port)
+	srv.ConfigureRoutes()
+	if err := srv.Start(); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
