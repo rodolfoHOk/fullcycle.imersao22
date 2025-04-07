bacde69b4d174c9b3f22cd12f10b7f25317af343
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-04-07 13:32:58 -0300

    feat: create account service

---
+++ go-gateway-api/internal/service/account_service.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"github.com/rodolfoHOk/fullcycle.imersao22/go-gateway-api/internal/domain"
+	"github.com/rodolfoHOk/fullcycle.imersao22/go-gateway-api/internal/dto"
+)
+
+type AccountService struct {
+	repository domain.AccountRepository
+}
+
+func NewAccountService(repository domain.AccountRepository) *AccountService {
+	return &AccountService{
+		repository: repository,
+	}
+}
+
+func (s *AccountService) Create(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIKey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) FindByAPIKey(apiKey string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) FindByID(id string) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (s *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = s.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
